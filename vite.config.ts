2e70ef570f59bd42fe36eb0486572be526f09740
deltarcm2025 <rb@deltarcm.com>
2026-03-12 14:47:03 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Use this one!
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/reconciliation-report-1/', 
