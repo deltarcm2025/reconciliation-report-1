43e90a6e84f3e25ee5731ff2cf9e2f6699172e4b
deltarcm2025 <rb@deltarcm.com>
2026-03-12 15:08:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Add Tailwind v4 plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Run Tailwind v4
+  ],
   base: '/reconciliation-report-1/',
 })
